84cfa8200f23fb7cc491676349a849500873f08a
unknown <vgrakesh527@gmail.com>
2023-01-08 01:50:48 +0530

    bug fix

--- Viper/snake_game/model.py
+++ Viper/snake_game/model.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import os
 
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
